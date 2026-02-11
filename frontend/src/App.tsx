8936fd2fe63b52f91206bfc9db5af5c76e34d7b0
itxadii <diamondgamerz49@gmail.com>
2026-02-12 00:18:28 +0530

    Improved project structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Navbar from './components/landingpage/Navbar'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+    <Navbar />
+    </>
+    
   )
 }
 
