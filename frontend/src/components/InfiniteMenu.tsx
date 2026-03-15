38af1f22ef7c47bae5aaa36eca0d27dc6021a3ea
itxadii <diamondgamerz49@gmail.com>
2026-03-16 00:16:26 +0530

    improved ui

--- frontend/src/components/InfiniteMenu.tsx
+++ frontend/src/components/InfiniteMenu.tsx
@@ -1,4 +1,4 @@
-import { type FC, useRef, useState, useEffect } from 'react';
+import { type FC, useRef, useState, useEffect, type MutableRefObject } from 'react';
 import { mat4, quat, vec2, vec3 } from 'gl-matrix';
 import './InfiniteMenu.css';
 
